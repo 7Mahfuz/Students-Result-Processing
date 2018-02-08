69eceb4bad57981e7b419623c4b371cf918a45a9
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-02-09 02:08:16 +0600

    Student Result Processing Web Application Starts

---
+++ Student Result Processing Web/Student Result Processing Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Student_Result_Processing_Web.MvcApplication" Language="C#" %>
---
+++ Student Result Processing Web/Student Result Processing Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Student_Result_Processing_Web.Startup))]
+namespace Student_Result_Processing_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
